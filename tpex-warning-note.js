68860ef6559c02790be2115b8de13ff8db0313c6
ooopiz <r.huang850@gmail.com>
2023-07-04 17:13:13 +0800

    Add tpex warning note

---
+++ tpex-warning-note.js
@@ -0,0 +1,23 @@
+import { apiCall, sendNotify } from "./lib/common.js";
+
+const main = async () => {
+  const stocks = await apiCall('https://www.tpex.org.tw/openapi/v1/tpex_trading_warning_note');
+
+  const noticeList = stocks.map((stock) => {
+    const {
+      SecuritiesCompanyCode, CompanyName, AccumulationSituation,
+      ...others
+    } = stock
+  
+    return `${CompanyName}, ${AccumulationSituation}`
+  })
+
+  const title='上櫃公布注意累計次數異常資訊';
+  const d = new Date();
+  const noticeMessage = `${title} ${d.toLocaleString()}\n${noticeList.join("\n")}`;
+  // console.log(noticeMessage)
+
+  await sendNotify(noticeMessage);
+}
+
+main();
